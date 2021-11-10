5fb23a73c2526d739afeb80107aad3fc364f9882
Ashley Williams <ashleyswilliams01@gmail.com>
2021-11-10 08:29:20 -0700

    adds both depthFirst solutions

---
+++ Reactos/DepthFirstPreOrderTreeTraversal.js
@@ -0,0 +1,54 @@
+// Today you will write a series of iterator functions for trees.
+// breadthFirst, depthFirstPreOrder, depthFirstPostOrder (if time allows)
+// Each of these functions will take a node of the tree and a callback. The function will iterate through the child nodes, calling the callback function on each of them. The difference between them is the order in which they iterate.
+
+// I split this up into three separate files.
+
+function node(value) {
+  return {
+    value,
+    children: [],
+  };
+}
+var a = node("a");
+var b = node("b");
+var c = node("c");
+var d = node("d");
+var e = node("e");
+var f = node("f");
+var g = node("g");
+var h = node("h");
+var i = node("i");
+var j = node("j");
+var k = node("k");
+var l = node("l");
+var m = node("m");
+
+a.children.push(b, c, d);
+b.children.push(e);
+e.children.push(k, l);
+c.children.push(f, g, h);
+h.children.push(m);
+d.children.push(i, j);
+
+//                A
+//            /   |    \
+//          B     C     D
+//         |    / | \   /\
+//         E   F  G H  I  J
+//        /\        |
+//       K L        M
+
+// doesn't have lefts and rights, just array of children from left to right
+
+// Time complexity: O(n)
+// Space complexity: O(1)
+
+function depthFirstPreOrder(node, callback) {
+  // process current node
+  callback(node.value);
+  // call depthFirst on each child
+  node.children.forEach((child) => {
+    depthFirstPreOrder(child, callback);
+  });
+}
--- ReadMe.md
+++ ReadMe.md
@@ -3,5 +3,8 @@
 Redoing all REACTOs from Grace Hopper
 
 ## Notes
+
 TwoNumSum - Successfully completed brute force and pointers approach unprompted.
 MultiplicativePersistence - First instinct was recursive. Worked, but not clean.
+BreadthFirst - Tried to complicate it with recursion, but knew that I needed to use a queue. Solution made sense.
+DepthFirst - Was unable to get close to a solution.
