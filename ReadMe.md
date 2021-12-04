991f6b872a003a5af53d21d45ebd30e667c4099c
Ashley Williams <ashleyswilliams01@gmail.com>
2021-12-03 18:15:11 -0700

    updates ReadMe after depthFirst retries

--- ReadMe.md
+++ ReadMe.md
@@ -7,7 +7,7 @@ Redoing all REACTOs from Grace Hopper
 TwoNumSum - Successfully completed brute force and pointers approach unprompted.
 MultiplicativePersistence - First instinct was recursive. Worked, but not clean.
 BreadthFirst - Tried to complicate it with recursion, but knew that I needed to use a queue. Solution made sense. Second try no issues.
-DepthFirst - + Was unable to get close to a solution.
+DepthFirst - Was unable to get close to a solution. After revisit, was able to solve immediately.
 Stock - + Brute force first try no issues. Optimized worked backwards from solution.
 Palindrome - + Brute force came very quickly. Struggled with recursive solution. Still not sure why we need a return statement there.
 StringSearch - + Understood problem, could only return last instance of complete needle, not first. Worked backwards from solution after 2 attempts.
--- deleteme.js
+++ deleteme.js
@@ -1,48 +1,45 @@
-// function node(value) {
-//   return {
-//     value,
-//     children: [],
-//   };
-// }
-// var a = node("a");
-// var b = node("b");
-// var c = node("c");
-// var d = node("d");
-// var e = node("e");
-// var f = node("f");
-// var g = node("g");
-// var h = node("h");
-// var i = node("i");
-// var j = node("j");
-// var k = node("k");
-// var l = node("l");
-// var m = node("m");
+function node(value) {
+  return {
+    value,
+    children: [],
+  };
+}
+var a = node("a");
+var b = node("b");
+var c = node("c");
+var d = node("d");
+var e = node("e");
+var f = node("f");
+var g = node("g");
+var h = node("h");
+var i = node("i");
+var j = node("j");
+var k = node("k");
+var l = node("l");
+var m = node("m");
 
-// a.children.push(b, c, d);
-// b.children.push(e);
-// e.children.push(k, l);
-// c.children.push(f, g, h);
-// h.children.push(m);
-// d.children.push(i, j);
+a.children.push(b, c, d);
+b.children.push(e);
+e.children.push(k, l);
+c.children.push(f, g, h);
+h.children.push(m);
+d.children.push(i, j);
 
-// //                A
-// //            /   |    \
-// //          B     C     D
-// //         |    / | \   /\
-// //         E   F  G H  I  J
-// //        /\        |
-// //       K L        M
+//                A
+//            /   |    \
+//          B     C     D
+//         |    / | \   /\
+//         E   F  G H  I  J
+//        /\        |
+//       K L        M
 
-// function depthFirstPostOrder(node, cb) {
+function depthFirstPostOrder(node, cb) {
+  node.children.forEach((child) => depthFirstPostOrder(child, cb));
+  cb(node);
+}
 
-// }
+const callback = (node) => {
+  console.log(node.value);
+};
 
-// Return the first index of the first appearance of one string (the needle) inside of another string (the haystack).
-
-// Input, needle and haystack
-// Output first index of first appearance
-
-// EXAMPLES
-// ('or', 'hello world'); // should return 7
-// ('hello world', 'or'); // should return -1
-
+depthFirstPostOrder(a, callback);
